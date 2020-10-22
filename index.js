702e6077cc0aadd3fd115837a207776746a75961
davidvanr21 <50579850+davidvanr21@users.noreply.github.com>
2020-10-22 16:11:09 +0200

    code exercise and documentFiles added

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-console.log('Hello world')
+// console.log('Fun Fun Function - Animals')
+
+// var animals = [
+//     {name: 'Fluffykins',    species: 'rabbit'},
+//     {name: 'Caro',          species: 'dog'},
+//     {name: 'Hamilton',      species: 'dog'},
+//     {name: 'Harold',        species: 'fish'},
+//     {name: 'Ursula',        species: 'cat'},
+//     {name: 'Jimmy',         species: 'fish'},
+// ]
+
+// var names = animals.map((animal) => animal.name)
+
+// console.log(names)
+
+
+
+// console.log(dogs.map(dog => dog.name))
+
+console.log('David')
+
+var orders = [
+    { amount: 250 },
+    { amount: 400 },
+    { amount: 100 },
+    { amount: 325 }
+]
+
+var totalAmount = orders.reduce((sum, order) => sum + order.amount, 0)
+
+console.log(totalAmount);
